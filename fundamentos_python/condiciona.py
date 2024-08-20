52e2e4bc530c9e571df98e2f7634eabc91d997b9
CarlosMoreira2021 <oliv.carlosmoreira@gmail.com>
2024-08-20 15:16:36 -0300

    condicionais

---
+++ fundamentos_python/condiciona.py
@@ -0,0 +1,6 @@
+cafe_coado = input(int('[1] Cafe coado: '))
+cafe_expresso = input(int('[2] Cafe expresso: '))
+capuccino = input(int('[3] Capucciono: '))
+chocolate = input(int('[4] Chocolate: '))
+
+
