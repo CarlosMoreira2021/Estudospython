281baac6c952b57cae033d76d724d716290e1242
CarlosMoreira2021 <oliv.carlosmoreira@gmail.com>
2024-08-20 16:14:13 -0300

    commit final

--- fundamentos_python/condiciona.py
+++ fundamentos_python/condiciona.py
@@ -1,6 +1,41 @@
-cafe_coado = input(int('[1] Cafe coado: '))
-cafe_expresso = input(int('[2] Cafe expresso: '))
-capuccino = input(int('[3] Capucciono: '))
-chocolate = input(int('[4] Chocolate: '))
+def pedido_cafe():
+    total = 0  
+    
+    while True:
+        print('Escolha seu café:')
+        print(' [0] Cancelar pedido')
+        print(' [1] Café coado - R$ 3,00')
+        print(' [2] Café expresso - R$ 4,00')
+        print(' [3] Capuccino - R$ 5,00')
+        print(' [4] Chocolate - R$ 6,00')
+        
+        try:
+            pedido = int(input('Digite o número da sua escolha: ')) 
 
+            if pedido == 0:
+                print('Pedido cancelado.')
+                break  
+            elif pedido == 1:
+                print('Você escolheu um café coado')
+                total += 3.00  
+            elif pedido == 2:
+                print('Você escolheu um café expresso')
+                total += 4.00  
+            elif pedido == 3:
+                print('Você escolheu um capuccino')
+                total += 5.00  
+            elif pedido == 4:
+                print('Você escolheu um chocolate')
+                total += 6.00  
+            else:
+                print('Número incorreto. Por favor, escolha um número entre 0 e 4.')
+                continue  
+            repetir = input('Deseja fazer outro pedido? (s/n): ').lower()
+            if repetir != 's':
+                print(f'Obrigado! O total do seu pedido é R$ {total:.2f}.')
+                break  
 
+        except ValueError:
+            print('Entrada inválida. Por favor, insira um número.')
+
+pedido_cafe()
